36601b690a965fcba451798d8b02063bfc2c8656
Maxime Vidori <maxime.vidori@gmail.com>
2013-12-25 08:04:47 +0100

    install.sh initialization

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PROJECT="RulzUrFront"
+TOP_DIR=$(cd $(dirname "$0") && pwd)
+
+docker_create () {
+  docker build -t="front" -rm=true .
+}
+
+docker_run () {
+  docker run -d -p 80 front
+}
+
+repository_initialization () {
+  sudo apt-get install -y node-js
+  sudo npm install -g grunt-cli
+  npm install
+}
+
+main () {
+  repository_initialization
+}
+
+main
