7161e74e01ac9d56e839b4f3d13dc7c36f8a0ff9
Maxime Vidori <maxime.vidori@gmail.com>
2015-10-04 19:26:01 +0200

    Add scss prepocessing and js creation

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,39 @@ var gulp = require('gulp');
 var del = require('del');
 var sass = require('gulp-sass');
 var sequence = require('gulp-sequence');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var merge = require('merge2');
 
-gulp.task('sass', function () {
-  return gulp.src('src/style/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/style'));
+gulp.task('styles', function () {
+  var sass_stream = gulp.src('src/style/**/*.scss')
+    .pipe(sass().on('error', sass.logError));
+
+  var normalize = gulp.src('node_modules/normalize.css/normalize.css');
+
+  return merge(normalize, sass_stream)
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css'));
+
+});
+
+gulp.task('scripts', function () {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('src/style/**/*.scss', ['sass']);
+  gulp.watch('src/style/**/*.scss', ['styles']);
 });
-
-gulp.task('build', sequence('clean', 'sass'));
 
 gulp.task('clean', function () {
   return del(['dist/**/*']);
 });
+
+gulp.task('build', sequence('clean', ['styles', 'scripts']));
+
